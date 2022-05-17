eefa0077bac3cc3cbdb79636d689957680049015
zhanghaocheng <771977492@qq.com>
2022-05-17 09:49:58 +0800

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("yong9ai.com");
+        System.out.println("infsec.cn");
+        System.out.println("com.yong9ai");
+    }
+}
